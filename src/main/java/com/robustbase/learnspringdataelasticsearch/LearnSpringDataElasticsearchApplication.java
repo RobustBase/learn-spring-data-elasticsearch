e4478fd8a20bcc8a9cf3adbb73742ac4b87b0e13
Atul Dwivedi <atul16dwivedi@gmail.com>
2020-07-22 22:47:01 +0530

    initial commit

---
+++ src/main/java/com/robustbase/learnspringdataelasticsearch/LearnSpringDataElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.robustbase.learnspringdataelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringDataElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringDataElasticsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robustbase/learnspringdataelasticsearch/LearnSpringDataElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robustbase.learnspringdataelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringDataElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
